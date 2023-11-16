cb3acdf9025f6364394242d1ed3822264f20f7cb
Vishal-m <vishalmahesh.study@gmail.com>
2023-11-17 00:37:45 +0530

    fixed create page ui issue -GRID

--- src/app/(teacher)/(routes)/teacher/courses/create/[courseId]/page.tsx
+++ src/app/(teacher)/(routes)/teacher/courses/create/[courseId]/page.tsx
@@ -60,16 +60,20 @@ const CourseCreatePage = async ({
     <div>
       <div className="container pt-6">
         <div>
-          <h1 className="text-4xl font-bold">Create a new course</h1>
+          <h1 className="text-4xl font-bold md:text-5xl">
+            Create a new course
+          </h1>
           <p className="text-sm mt-2 text-muted-foreground">
             Fields remaining {completed}/{tasks.length}
           </p>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 md:mt-20 mt-12 pl-6 space-x-20">
+        <div className="grid grid-cols-1 md:grid-cols-2 md:mt-20 mt-5 gap-x-10">
           <div className="flex flex-col gap-10">
             <div className="flex items-center gap-3">
               <IconBadge icon={LayoutDashboard} status={false} />
-              <h1 className="font-bold">Customize your course</h1>
+              <h1 className="font-bold md:text-xl lg:text-2xl">
+                Customize your course
+              </h1>
             </div>
             <TitleForm
               initialValue={`${course.title}`}
@@ -89,10 +93,12 @@ const CourseCreatePage = async ({
               options={categoryData}
             />
           </div>
-          <div className="flex flex-col gap-10">
+          <div className="flex flex-col gap-10 mt-10 md:mt-0">
             <div className="flex items-center gap-2">
               <IconBadge icon={ListTodo} status={false} />
-              <h1 className="font-bold">Course chapters</h1>
+              <h1 className="font-bold md:text-xl lg:text-2xl">
+                Course chapters
+              </h1>
             </div>
 
             <ChapterSection
@@ -102,7 +108,9 @@ const CourseCreatePage = async ({
             <div>
               <div className="flex items-center gap-2">
                 <IconBadge icon={DollarSign} status={false} />
-                <h1 className="font-bold">Sell your course</h1>
+                <h1 className="font-bold md:text-xl lg:text-2xl">
+                  Sell your course
+                </h1>
               </div>
               <PriceSection
                 initialValue={course.price}
@@ -113,7 +121,7 @@ const CourseCreatePage = async ({
             <div>
               <div className="flex items-center gap-2 md:pb-6">
                 <IconBadge icon={File} status={false} />
-                <h1 className="font-bold">
+                <h1 className="font-bold md:text-xl lg:text-2xl">
                   Does your students need any resources to complete this course
                 </h1>
               </div>
