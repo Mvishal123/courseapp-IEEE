b0c0d00d42b1b7c85dc41958d2878239ee085a7d
Vishal-m <vishalmahesh.study@gmail.com>
2023-11-18 02:06:12 +0530

    fixed serverSideSessionFetch problem atlast :)

--- src/app/(teacher)/(routes)/teacher/courses/create/[courseId]/chapter/[chapterId]/page.tsx
+++ src/app/(teacher)/(routes)/teacher/courses/create/[courseId]/chapter/[chapterId]/page.tsx
@@ -1,37 +1,65 @@
-"use client";
-import React from "react";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
+import { getSession, useSession } from "next-auth/react";
+import { redirect, useRouter } from "next/navigation";
 import { ArrowLeft } from "lucide-react";
+import { connectDb } from "@/lib/db";
+import toast from "react-hot-toast";
+import { Chapter, MuxData } from "@/models/index";
+import Link from "next/link";
+import { getServerSession } from "next-auth";
+import { handler } from "@/app/api/auth/[...nextauth]/route";
 
-const ChapterDetailsSection = ({
+connectDb();
+
+const ChapterDetailsSection = async ({
   params,
 }: {
   params: { courseId: string; chapterId: string };
 }) => {
-  const { data: session } = useSession();
-  const router = useRouter();
+  const session = await getServerSession(handler);
 
-  if (!session?.user) {
-    router.push("/teacher");
+  if (!session) {
+    return redirect("/signin");
   }
+
+  const chapter = await Chapter.findOne({
+    _id: params.chapterId,
+    courseId: params.courseId,
+  }).populate("muxData");
+
+  console.log("chapter", chapter);
+
+  if (!chapter) {
+    toast.error("Something went wrong");
+    console.log("chapter not found");
+
+    return redirect(`/teacher/courses/create/${params.courseId}`);
+  }
+
+  const muxData = await MuxData.find({ chapterId: params.chapterId });
+
+  const requiredFields = [chapter.title, chapter.description, chapter.videoUrl];
+
+  const isCompleted = requiredFields.filter(Boolean).length;
+
+  const requiredRender = `Complete all fields (${isCompleted}/${requiredFields.length})`;
 
   return (
     <div className="p-6">
-        <div className="">
-            <div className="flex items-center justify-between w-full">
-                <div>
-                    <div>
-                        <ArrowLeft />
-                    </div>
-                    <p>
-                        back to the course setup
-                    </p>
-                </div>
+      <div className="">
+        <div className="flex items-center justify-between w-full text-sm">
+          <Link href={`/teacher/courses/create/${params.courseId}`}>
+            <div className="flex gap-2 items-center text-slate-800 hover:text-slate-600 cursor-pointer">
+              <ArrowLeft className="w-4 h-4" />
+              <p>back to the course setup</p>
             </div>
+          </Link>
         </div>
+        <div className="mt-4">
+          <h1>Create chapter</h1>
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default ChapterDetailsSection;
