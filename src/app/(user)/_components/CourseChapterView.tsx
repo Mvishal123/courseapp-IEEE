931de2c025864bccdcde81f703fc97548dcedb32
Vishal-m <vishalmahesh.study@gmail.com>
2023-11-28 16:57:37 +0530

    Chapter collapsible added

--- src/app/(user)/_components/CourseChapterView.tsx
+++ src/app/(user)/_components/CourseChapterView.tsx
@@ -1,15 +1,35 @@
-import React from "react";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import { ArrowDown, ChevronDown } from "lucide-react";
 
 type Chapters = {
-  chapters: Object[];
+  chapters: { title: string, description: string}[];
 };
 
 const CourseChapterView = ({ chapters }: Chapters) => {
   return (
     <div>
-      {chapters.map((chapter: any, index: number) => (
-        <h1>{chapter.title}</h1>
-      ))}
+      <ul>
+        {chapters.map((chapter, index) => (
+          <div key={index}>
+            <Collapsible>
+            <div className="md:w-1/2 w-full flex bg-slate-100 p-2 rounded-sm justify-between">
+                <h1>{chapter.title}</h1>
+                <CollapsibleTrigger>
+                    <ChevronDown/>
+                </CollapsibleTrigger>
+            </div>
+              
+              <CollapsibleContent>
+              <div className="p-2 border md:w-1/2 w-full rounded-sm" dangerouslySetInnerHTML={{ __html: chapter.description }} />
+              </CollapsibleContent>
+            </Collapsible>
+          </div>
+        ))}
+      </ul>
     </div>
   );
 };
