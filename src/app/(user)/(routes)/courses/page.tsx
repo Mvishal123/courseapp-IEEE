e259ea7f8a35f77abffb635bcd1185682ab7dbba
Vishal-m <vishalmahesh.study@gmail.com>
2023-12-03 13:06:42 +0530

    fixed all course render problem

--- src/app/(user)/(routes)/courses/page.tsx
+++ src/app/(user)/(routes)/courses/page.tsx
@@ -26,81 +26,75 @@ const page = async ({ searchParams }: SearchParamsProps) => {
   console.log(searchParams.categoryId);
   console.log(searchParams.title);
 
-  if (!session) {
-    return <div></div>;
-  } else {
-    console.log("SESSION", session.user.email);
+  let query: query = {
+    isPublished: true,
+  };
 
-    let query: query = {
-      isPublished: true,
+  if (searchParams.categoryId) {
+    query.category = searchParams.categoryId;
+  }
+
+  if (searchParams.title) {
+    const titleQuery = {
+      $or: [
+        { title: { $regex: new RegExp(searchParams.title, "i") } },
+        { description: { $regex: new RegExp(searchParams.title, "i") } },
+      ],
     };
 
-    if (searchParams.categoryId) {
-      query.category = searchParams.categoryId;
-    }
+    console.log("titleQuery: ", titleQuery);
+    query = { ...query, ...titleQuery };
+  }
 
-    if (searchParams.title) {
-      const titleQuery = {
-        $or: [
-          { title: { $regex: new RegExp(searchParams.title, "i") } },
-          { description: { $regex: new RegExp(searchParams.title, "i") } },
-        ],
-      };
+  const courses = await Course.find(query);
+  console.log("COURSES", courses);
 
-      console.log("titleQuery: ", titleQuery);
-      query = { ...query, ...titleQuery };
-    }
+  const user = await User.findOne({ email: session?.user?.email });
+  console.log("USER", user);
 
-    const courses = await Course.find(query);
-    console.log("COURSES", courses);
+  let renderCourses;
+  {
+    user.mycourses
+      ? (renderCourses = courses.filter(
+          (course: any) => !user.mycourses.includes(course._id)
+        ))
+      : (renderCourses = courses);
+  }
 
-    const user = await User.findOne({ email: session?.user?.email });
-    console.log("USER", user);
+  const categories = await CourseCategory.find({});
 
-    let renderCourses;
-    {
-      user.mycourses
-        ? (renderCourses = courses.filter(
-            (course: any) => !user.mycourses.includes(course._id)
-          ))
-        : (renderCourses = courses);
-    }
+  return (
+    <main className="">
+      <div className="lg:hidden block w-1/2 mx-auto">
+        <Searchbar />
+      </div>
+      <div className="flex gap-3 overflow-x-scroll no-scrollbar px-3 mt-4">
+        {categories.map((category: any, index) => (
+          <CoursesCategorySlider
+            label={`${category.category}`}
+            value={`${category._id}`}
+            key={index}
+          />
+        ))}
+      </div>
+      <div className="container mt-6">
+        <h1 className="text-4xl font-extrabold text-center">All courses</h1>
+        <br />
+        <hr />
 
-    const categories = await CourseCategory.find({});
-
-    return (
-      <main className="">
-        <div className="lg:hidden block w-1/2 mx-auto">
-          <Searchbar />
-        </div>
-        <div className="flex gap-3 overflow-x-scroll no-scrollbar px-3 mt-4">
-          {categories.map((category: any, index) => (
-            <CoursesCategorySlider
-              label={`${category.category}`}
-              value={`${category._id}`}
-              key={index}
-            />
+        {courses.length === 0 && (
+          <div className="flex justify-center italic text-muted-foreground mt-3">
+            no courses
+          </div>
+        )}
+        <div className="grid grid-cols-1 md:grid-cols-4 mt-3 gap-8">
+          {renderCourses.map((course: any, i) => (
+            <CourseCard data={course.toObject()} key={i} />
           ))}
         </div>
-        <div className="container mt-6">
-          <h1 className="text-4xl font-extrabold text-center">All courses</h1>
-          <br />
-          <hr />
-
-          {courses.length === 0 && (
-            <div className="flex justify-center italic text-muted-foreground mt-3">
-              no courses
-            </div>
-          )}
-          <div className="grid grid-cols-1 md:grid-cols-4 mt-3 gap-8">
-            {renderCourses.map((course: any, i) => (
-              <CourseCard data={course.toObject()} key={i} />
-            ))}
-          </div>
-        </div>
-      </main>
-    );
-  }
+      </div>
+    </main>
+  );
 };
 
 export default page;
